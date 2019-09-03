c4ffbcafeef040b46a7d57c04e80cc6d5798e7e3
Sreya Chagarlamudi <schagarlamudi@cs.arizona.edu>
2019-09-03 10:17:51 -0700

    Added comments

--- net_client.py
+++ net_client.py
@@ -1,15 +1,16 @@
 import socket
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-addr = ("localhost", 15556)
-sock.connect(addr)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #initialize socket
+addr = ("localhost", 15556) #declare address and port of connection 
+sock.connect(addr) #connect to address and port
 msg =''
-#for i in range(100):
-#    msg += str(i)+'\n'
-while (True):
+
+
+while (True): #continue to accept replies with while loop 
     msg = input('Enter message to send to server: ')
-    sock.sendall(msg.encode())
+    sock.sendall(msg.encode()) #send message to server
     print('Sent message to server, waiting for reply')
-    newMsg = sock.recv(1024).decode()
-    print(newMsg)
+    newMsg = sock.recv(1024).decode() #decode reply
+    print(newMsg) #output reply from server 
+
 
--- net_server.py
+++ net_server.py
@@ -1,22 +1,22 @@
 import socket
 
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-addr = ("localhost", 15556)
-sock.bind(addr)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #initialize socket
+addr = ("localhost", 15556) #declare address and port
+sock.bind(addr) #server binds to address and port (client does not bind to address)
 
-sock.listen(5)
+sock.listen(5) #buffer in case connection fails--> number indicates # of failed connections allowed
 
 msg = ''
 
-(connectedSock, clientAddress) = sock.accept()
+(connectedSock, clientAddress) = sock.accept() #accepts connection, returns new socket (new socket= connectedSock)
 
 while (True):
     try:
-        msg = connectedSock.recv(1024).decode()
+        msg = connectedSock.recv(1024).decode() #decode msg from client 
         print('Received message: '+msg)
-    except(ConnectionAbortedError):
+    except(ConnectionAbortedError): #if socket hits end of data stream
         print('End of received message, closing socket.')
         connectedSock.close()
     reply = 'I got the mesage: '+msg
-    connectedSock.sendall(reply.encode())
+    connectedSock.sendall(reply.encode()) #send message back to client 
